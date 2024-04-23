0e539d6beaaf78cdcc2054b5b561f2a7d00bbf9f
Miguel <migueortiz0125@gmail.com>
2024-04-22 22:26:06 -0700

    navbar started

--- pricewise/components/Navbar.tsx
+++ pricewise/components/Navbar.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='w-full'>
+      <nav className='nav'>
+        <Link href="/" className='flex items-center gap-1'>
+          <Image
+            src='/assets/icons/logo.svg'
+            width={27}
+            height={27}
+            alt='logo'
+          />
+        </Link>
+      </nav>
+    </header>
   )
 }
 
