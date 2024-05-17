a2108be4214bf5ab6f860d4e7a46caae90abe567
Miguel <migueortiz0125@gmail.com>
2024-05-17 11:39:37 -0700

    product details page completed

--- pricewise/app/page.tsx
+++ pricewise/app/page.tsx
@@ -1,8 +1,13 @@
 import HeroCarousel from "@/components/HeroCarousel"
+import ProductCard from "@/components/ProductCard"
 import Searchbar from "@/components/Searchbar"
+import { getAllProducts } from "@/lib/actions"
 import Image from "next/image"
 
-const Home = () => {
+const Home = async () => {
+
+  const allProducts = await getAllProducts();
+
   return (
     <>
       <section className="px-6 md:px-20 py-24">
@@ -32,8 +37,8 @@ const Home = () => {
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {['Apple iPhone 15', 'Book', 'Sneakers'].map((product) => (
-            <div>{product}</div>
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
